c2b704cf10d22a07c59aaf86edb1c77c663f1c1f
Dustin Myers <dustint314@gmail.com>
2021-02-08 10:56:04 -0700

    wip

--- review/src/App.js
+++ review/src/App.js
@@ -2,11 +2,13 @@ import React, { useState, Component } from "react";
 
 // class App extends React.Component {
 class App extends Component {
-  return (
-    <div>
-      <h1>Hello Warren</h1>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <h1>Hello Warren</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
